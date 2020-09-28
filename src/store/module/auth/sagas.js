df49454fd003527324f901dadc8462642fd27896
unknown <jeffynho32@gmail.com>
2020-09-28 16:03:49 -0300

    feat(store): Definindo feature para iniciar uma sessão no sistema (signIn)

--- src/store/module/auth/sagas.js
+++ src/store/module/auth/sagas.js
@@ -1,10 +1,49 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '../../../services/api';
+
+import { signFailure, signInSuccess } from './actions';
 
 import history from '../../../services/history';
 
-export function* signIn({ payload }) {}
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
 
-export function setToken({ payload }) {}
+    const response = yield call(api.post, 'session/companies', {
+      email,
+      password,
+    });
+
+    const { user, token } = response.data;
+
+    if (!user.company.access) {
+      toast.error('Acesso restrito. Entra em contato com a equipe SkuJob.');
+      yield put(signFailure());
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(user, token));
+
+    history.push('/main');
+  } catch (err) {
+    toast.error(
+      'Erro ao tentar acessar o sistema. Por favor verifique seus dados'
+    );
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
 
 export function signOut() {
   history.push('/');
