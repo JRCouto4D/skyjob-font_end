8c19618e49b94bf745cefc4bd28b165eff27f49a
unknown <jeffynho32@gmail.com>
2020-09-28 11:24:38 -0300

    feat: Definindo estilos para tela de signIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Input, Form } from '@rocketseat/unform';
+import { MdPerson, MdLock } from 'react-icons/md';
+import logo from '../../assets/logo.png';
 
-// import { Container } from './styles';
+import { Container, InputBlock } from './styles';
 
 function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <Container>
+      <img src={logo} alt="SkyJob" />
+
+      <Form onSubmit={() => {}}>
+        <InputBlock>
+          <MdPerson color="#cb0000" size={20} />
+          <Input
+            name="email"
+            type="email"
+            placeholder="Digite seu email"
+            autoComplete="off"
+          />
+        </InputBlock>
+        <InputBlock>
+          <MdLock color="#cb0000" size={20} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Sua senha secreta"
+          />
+        </InputBlock>
+
+        <button type="submit">Acessar</button>
+      </Form>
+    </Container>
+  );
 }
 
 export default SignIn;
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 380px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 350px;
+    margin-left: 35px;
+    margin-top: 15px;
+
+    input {
+      padding: 10px;
+
+      border: none;
+      background: none;
+      color: ${darken(0.1, '#cb0000')};
+
+      ::placeholder {
+        color: #cb0000;
+      }
+    }
+
+    button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 45px;
+      margin-top: 10px;
+      border-radius: 4px;
+      background: #cb0000;
+      border: none;
+
+      color: #fff;
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+`;
+
+export const InputBlock = styled.div`
+  width: 345px;
+  border-radius: 4px;
+  border: 2px solid #cb0000;
+
+  margin-bottom: 10px;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
+`;
