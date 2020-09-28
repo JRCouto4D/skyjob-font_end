84c46999b23eee610d06c9b93cfb486227d198df
unknown <jeffynho32@gmail.com>
2020-09-28 16:04:40 -0300

    feat: Pre configuração da página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Container>
+      <h1>Main</h1>
+    </Container>
+  );
 }
 
 export default Main;
