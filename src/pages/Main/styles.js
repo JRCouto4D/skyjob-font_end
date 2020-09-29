ee68e04fc8d86fca79ba8dca3c72e069c09ebdb9
unknown <jeffynho32@gmail.com>
2020-09-28 22:59:05 -0300

    refactor: Correção nos estilos

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #7159c1;
+  background: #eee;
 `;
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,17 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { store } from '../store';
 
 import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
 
-import { store } from '../store';
-
 export default function RouteWrapper({
   component: Component,
-  isPrivate,
+  isPrivate = false,
   ...rest
 }) {
   const { signed } = store.getState().auth;
@@ -24,15 +23,15 @@ export default function RouteWrapper({
     return <Redirect to="/main" />;
   }
 
-  const Layout = signed ? DefaultLayout : AuthLayout;
+  const Layouts = signed ? DefaultLayout : AuthLayout;
 
   return (
     <Route
       {...rest}
       render={(props) => (
-        <Layout>
+        <Layouts>
           <Component {...props} />
-        </Layout>
+        </Layouts>
       )}
     />
   );
