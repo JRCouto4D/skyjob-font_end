0b8ea4fba0126eed090351005f72ff7f7bdbb439
unknown <jeffynho32@gmail.com>
2020-09-30 15:57:33 -0300

    feat: Definindo estilos para o radapé da aplicação

---
+++ src/components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { MdContacts, MdEmail, MdPhone } from 'react-icons/md';
+
+import logo from '../../assets/logo2.png';
+
+import { Container, BoxRight, BoxLeft, BoxContacts } from './styles';
+
+function Footer() {
+  return (
+    <Container>
+      <img src={logo} alt="" />
+
+      <BoxRight>
+        <BoxLeft>
+          <MdContacts color="#FFF" size={25} />
+          <h2>CONTATOS</h2>
+        </BoxLeft>
+
+        <div />
+
+        <BoxContacts>
+          <div>
+            <MdEmail color="#fff" size={15} />
+            <span>skyjob@email.com</span>
+          </div>
+          <div>
+            <MdPhone color="#fff" size={15} />
+            <span>(77) 98120-0675</span>
+          </div>
+        </BoxContacts>
+      </BoxRight>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 60px;
+  background: #ab0000;
+
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    display: block;
+    width: 240px;
+    height: 50px;
+  }
+`;
+
+export const BoxRight = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const BoxLeft = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  padding-right: 20px;
+  margin-right: 20px;
+  border-right: 2px solid #fff;
+
+  h2 {
+    color: #fff;
+    font-size: 14px;
+    margin-left: 5px;
+  }
+`;
+export const BoxContacts = styled.div`
+  margin-right: 15px;
+
+  && div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    span {
+      color: #fff;
+      margin-left: 5px;
+    }
+  }
+`;
