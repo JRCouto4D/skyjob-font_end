e3f8a8107a6c468bc11defe88876cb7c9b401da4
unknown <jeffynho32@gmail.com>
2020-10-13 16:35:23 -0300

    feat(pages/Catalog/Providers): Definindo estilos e comportamentos da página que vai lidar com listagem de dados de fornecedores

--- src/pages/Catalog/Providers/index.js
+++ src/pages/Catalog/Providers/index.js
@@ -1,11 +1,250 @@
-import React from 'react';
+/* eslint-disable no-nested-ternary */
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Input } from '@rocketseat/unform';
+import { toast } from 'react-toastify';
+import {
+  MdSearch,
+  MdFilterList,
+  MdCreate,
+  MdDeleteForever,
+  MdFastRewind,
+  MdFastForward,
+} from 'react-icons/md';
+import { FaSpinner } from 'react-icons/fa';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
-import { Container } from './styles';
+import api from '../../../services/api';
+import history from '../../../services/history';
+
+import {
+  Container,
+  Content,
+  BoxLeft,
+  BoxSearch,
+  Block,
+  BoxRight,
+  TableProviders,
+  LineTable,
+  Loading,
+  BoxNavigation,
+} from './styles';
 
 function Providers() {
+  const { company } = useSelector((state) => state.user.profile);
+
+  const [search, setSearch] = useState('');
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [prePage, setPrePage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [providers, setProviders] = useState([]);
+
+  const handleFilter = useCallback(async () => {}, []);
+
+  const loadProviders = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const response = await api.get(`/company/${company.id}/providers/list`, {
+        params: {
+          page,
+          name: search,
+        },
+      });
+
+      setProviders(response.data.providers);
+      setTotal(response.data.total);
+      setPrePage(Math.ceil(response.data.total / 5));
+      setLoading(false);
+    } catch (err) {
+      toast.error('Algo deu errado, por favor tente mais tarde');
+      history.push('/main');
+    }
+  }, [page, search, company]);
+
+  const deleteProvider = useCallback(
+    async (id) => {
+      confirmAlert({
+        title: 'Confirmar remoção',
+        message: 'Deseja remover este fornecedor?',
+        buttons: [
+          {
+            label: 'Sim',
+            onClick: async () => {
+              try {
+                await api.delete(
+                  `company/${company.id}/providers/${id}/delete`
+                );
+                toast.success('O Fornecedor foi deletado com sucesso!');
+                loadProviders();
+              } catch (err) {
+                toast.error('Algo deu errado, por favor tente mais tarde');
+                history.push('/providers');
+              }
+            },
+          },
+          {
+            label: 'Não',
+            onClick: () => history.push('/providers'),
+          },
+        ],
+      });
+    },
+    [loadProviders]
+  );
+
+  useEffect(() => {
+    loadProviders();
+  }, [loadProviders]);
+
+  function nextPage() {
+    setPrePage(Math.ceil(total / 5));
+    setPage(page < prePage ? page + 1 : page);
+
+    loadProviders();
+  }
+
+  function prevPage() {
+    setPage(page >= 2 ? page - 1 : page);
+
+    loadProviders();
+  }
+
+  const memoList = useMemo(() => {
+    return (
+      <TableProviders>
+        <li className="header">
+          <strong>DESCRIÇÃO</strong>
+          <strong>REPRESENTANTE</strong>
+          <strong>Email</strong>
+          <strong>TELEFONE</strong>
+          <strong>CELULAR</strong>
+          <strong>ATIVO</strong>
+          <strong>AÇÕES</strong>
+        </li>
+
+        {providers.map((item) => {
+          return (
+            <LineTable>
+              <strong>
+                {item.name.length > 38
+                  ? `${item.name.substr(0, 38)}...`
+                  : item.name}
+              </strong>
+
+              <strong>
+                {item.representative
+                  ? item.representative.length > 38
+                    ? `${item.representative.substr(0, 38)}...`
+                    : item.representative
+                  : ''}
+              </strong>
+
+              <strong>
+                {item.email
+                  ? item.email.length > 38
+                    ? `${item.email.substr(0, 38)}...`
+                    : item.email
+                  : ''}
+              </strong>
+
+              <strong>{item.telephone}</strong>
+
+              <strong>{item.cell_phone}</strong>
+
+              <strong>{item.active ? 'SIM' : 'NÃO'}</strong>
+
+              <div className="boxActions">
+                <button
+                  type="button"
+                  onClick={() => {
+                    history.push('/providers/newProvider', {
+                      item,
+                    });
+                  }}
+                >
+                  <MdCreate color="#333" size={20} />
+                </button>
+
+                <button type="button" onClick={() => deleteProvider(item.id)}>
+                  <MdDeleteForever color="#ab0000" size={20} />
+                </button>
+              </div>
+            </LineTable>
+          );
+        })}
+      </TableProviders>
+    );
+  }, [providers, deleteProvider]);
+
   return (
     <Container>
-      <h1>Providers</h1>
+      <Content>
+        <div>
+          <header>
+            <BoxLeft>
+              <div>
+                <strong>CATÁLOGO</strong>
+                <h1>FORNECEDORES</h1>
+              </div>
+            </BoxLeft>
+            <BoxSearch>
+              <MdSearch size={20} color="#ccc" />
+              <Block />
+              <Input
+                type="text"
+                id="search"
+                name="search"
+                placeholder="O que voce está procurando"
+                autoCapitalize="off"
+                autoComplete="off"
+                onChange={(e) => {
+                  setPage(1);
+                  setSearch(e.target.value);
+                }}
+              />
+            </BoxSearch>
+            <BoxRight>
+              <button onClick={handleFilter} type="button">
+                <MdFilterList color="#ab0000" size={50} />
+              </button>
+              <Link to="/providers/newProvider">
+                <span>ADICIONAR</span>
+                <strong>NOVO</strong>
+              </Link>
+            </BoxRight>
+          </header>
+
+          {loading ? (
+            <Loading>
+              <FaSpinner color="#ab0000" size={30} />
+            </Loading>
+          ) : providers.length <= 0 ? (
+            <h1
+              style={{ marginTop: 10 }}
+            >{`Nenhum resultado para "${search}".`}</h1>
+          ) : (
+            memoList
+          )}
+        </div>
+
+        <BoxNavigation>
+          <div>
+            <button onClick={prevPage} type="button" disabled={page === 1}>
+              <MdFastRewind size={20} color="#fff" />
+            </button>
+
+            <span>{`${page} de ${prePage}`}</span>
+
+            <button onClick={nextPage} type="button" disabled={page >= prePage}>
+              <MdFastForward size={20} color="#fff" />
+            </button>
+          </div>
+        </BoxNavigation>
+      </Content>
     </Container>
   );
 }
--- src/pages/Catalog/Providers/styles.js
+++ src/pages/Catalog/Providers/styles.js
@@ -1,3 +1,199 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { lighten } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  width: 80%;
+  height: 510px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 15px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+export const BoxLeft = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  > div {
+    display: flex;
+    flex-direction: column;
+    strong {
+      font-size: 16px;
+      color: #172c3d;
+    }
+    h1 {
+      color: #ab0000;
+    }
+  }
+`;
+export const BoxSearch = styled.div`
+  margin-left: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: left;
+  width: 100%;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  height: 40px;
+  padding: 0 10px;
+  input {
+    height: 22px;
+    width: 100%;
+    border: none;
+    border-left: 1px solid #ccc;
+    margin-left: 10px;
+    padding-left: 10px;
+    color: #999;
+    ::placeholder {
+      color: #ccc;
+    }
+  }
+`;
+
+export const Block = styled.div``;
+export const BoxRight = styled.div`
+  width: 350px;
+  margin-left: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  button {
+    background: none;
+    border: none;
+  }
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #ab0000;
+    width: 110px;
+    height: 40px;
+    margin-left: 20px;
+    border-radius: 4px;
+    :hover {
+      background: ${lighten(0.05, '#ab0000')};
+    }
+    strong {
+      color: #fff;
+      font-size: 16px;
+    }
+    span {
+      color: #fff;
+      font-size: 10px;
+    }
+  }
+`;
+
+export const TableProviders = styled.ul`
+  width: 100%;
+  display: grid;
+  grid-gap: 15px;
+  margin-top: 25px;
+  li.header {
+    margin-top: 3px;
+    display: grid;
+    grid-template-columns: 3fr 3fr 3fr 2fr 2fr 1fr 60px;
+    padding: 0 10px;
+    strong {
+      color: #333;
+      font-size: 12.5px;
+    }
+  }
+`;
+
+export const LineTable = styled.li`
+  margin-top: 3px;
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr 2fr 2fr 1fr 60px;
+  background: #eee;
+  width: 100%;
+  padding: 12px 10px;
+  align-items: center;
+  border-radius: 4px;
+  strong {
+    color: #666;
+    font-weight: normal;
+    font-size: 12px;
+  }
+  div.boxActions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 50px;
+    button {
+      background: none;
+      border: none;
+      :hover {
+        opacity: 0.7;
+      }
+    }
+  }
+`;
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg)
+  }
+  to{
+    transform: rotate(360deg)
+  }
+`;
+
+export const Loading = styled.div`
+  width: 100%;
+  height: 300px;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    animation: ${rotate} 1s linear infinite;
+  }
+`;
+
+export const BoxNavigation = styled.div`
+  display: flex;
+  div {
+    display: flex;
+    width: 150px;
+    align-items: center;
+    justify-content: space-between;
+    button {
+      background: #ab0000;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: none;
+      padding: 8px;
+      :disabled {
+        opacity: 0.4;
+        pointer-events: none;
+      }
+    }
+    span {
+      color: #666;
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+`;
