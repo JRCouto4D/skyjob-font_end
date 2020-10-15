11556f2b4d5aba2cc697461700a1be521cf9c067
unknown <jeffynho32@gmail.com>
2020-10-15 14:18:09 -0300

    feat: definindo estilos do componente fixo no pdv

---
+++ src/components/Box_point-sale/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { useSelector } from 'react-redux';
+import { MdSwapHoriz, MdSettings } from 'react-icons/md';
+
+import logo from '../../assets/logo3.png';
+
+import {
+  Container,
+  BoxLeft,
+  BoxActions,
+  BoxRight,
+  GoSell,
+  GoOperations,
+} from './styles';
+
+function BoxPointSale({ children, poup }) {
+  const { pdv } = useSelector((state) => state.statusPDV);
+
+  return (
+    <Container>
+      <BoxLeft>
+        <div>
+          <img src={logo} alt="logo" />
+
+          <div className="box-date">
+            <strong>
+              {format(new Date(), 'iiii', {
+                locale: pt,
+              }).toUpperCase()}
+            </strong>
+
+            <span>{format(new Date(), 'dd/MM/yyyy')}</span>
+          </div>
+
+          <div className="info-block">
+            <strong>VENDEDOR</strong>
+            <span>{pdv.user.name.toUpperCase()}</span>
+          </div>
+
+          <div className="info-block">
+            <strong>PDV</strong>
+            <span>{pdv.cash_register.description.toUpperCase()}</span>
+          </div>
+        </div>
+
+        <BoxActions>
+          <GoSell to="/main" color={poup ? '#ab0000' : '#333'}>
+            <MdSwapHoriz size={50} color="#fff" />
+            <strong>VENDER</strong>
+          </GoSell>
+
+          <GoOperations to="/main" color={poup ? '#333' : '#ab0000'}>
+            <MdSettings size={50} color="#fff" />
+            <strong>OPERAÇÕES</strong>
+          </GoOperations>
+        </BoxActions>
+      </BoxLeft>
+
+      <BoxRight>{children}</BoxRight>
+    </Container>
+  );
+}
+
+export default BoxPointSale;
+
+BoxPointSale.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element),
+};
+
+BoxPointSale.defaultProps = {
+  children: null,
+};
+
+BoxPointSale.propTypes = {
+  poup: PropTypes.bool,
+};
+
+BoxPointSale.defaultProps = {
+  poup: true,
+};
---
+++ src/components/Box_point-sale/styles.js
@@ -0,0 +1,119 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { lighten } from 'polished';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  background: #fff;
+  border-radius: 4px;
+  width: 90%;
+  height: 520px;
+  box-shadow: 0px 0px 6px 1px rgba(0, 0, 0, 0.1);
+`;
+
+export const BoxLeft = styled.div`
+  width: 150px;
+  height: 100%;
+  background: #333;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 140px;
+    margin-top: 5px;
+  }
+
+  div.box-date {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5px;
+
+    strong {
+      font-size: 18px;
+      color: #ddd;
+    }
+
+    span {
+      color: #ddd;
+      font-size: 16px;
+    }
+  }
+
+  div.info-block {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 30px 5px 0 5px;
+
+    strong {
+      font-size: 14px;
+      color: #999;
+    }
+
+    span {
+      font-size: 16px;
+      font-weight: bold;
+      text-align: center;
+      color: #fff;
+      margin-top: 5px;
+    }
+  }
+`;
+
+export const BoxActions = styled.div`
+  width: 100%;
+`;
+
+export const GoSell = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 120px;
+  background: ${(props) => props.color};
+
+  :hover {
+    background: ${(props) => lighten(0.1, `${props.color}`)};
+  }
+
+  strong {
+    font-size: 20px;
+    color: #fff;
+    margin-top: 3px;
+  }
+`;
+
+export const GoOperations = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 120px;
+  background: ${(props) => props.color};
+
+  :hover {
+    background: ${(props) => lighten(0.1, `${props.color}`)};
+  }
+
+  strong {
+    font-size: 18px;
+    color: #fff;
+    margin-top: 3px;
+  }
+`;
+
+export const BoxRight = styled.div`
+  width: calc(100% - 200px);
+  height: 100%;
+`;
