c3a588f74c855e72bb66aac60fb8a3bdf7c6223e
unknown <jeffynho32@gmail.com>
2020-10-23 23:01:02 -0300

    feat: Incluindo metodo para calculo de porcetagens

--- src/util/calcPercentage.js
+++ src/util/calcPercentage.js
@@ -4,3 +4,10 @@ export function percentage(a, b) {
   c /= a;
   return c.toFixed(2);
 }
+
+export function percentage2(a, b) {
+  const c = b.toFixed(2) * 100;
+  const d = c / a.toFixed(2);
+  const e = 100 - d;
+  return e.toFixed(1);
+}
