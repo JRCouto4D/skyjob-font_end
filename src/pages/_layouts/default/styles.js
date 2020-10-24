a008fb258b64405c10b194c6b818b30649839f3b
unknown <jeffynho32@gmail.com>
2020-10-23 22:58:30 -0300

    refactor: Correção nos estilos dos layouts pre-definidos

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: #eee;
-  overflow: auto;
+  overflow: hidden;
 `;
 
 export const Content = styled.div`
