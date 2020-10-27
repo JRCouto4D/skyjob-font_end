9cdc44056e34acd4f17960cf106f3201ae6f5028
unknown <jeffynho32@gmail.com>
2020-10-27 08:34:17 -0300

    feat: definindo estilos para componet Footer

--- src/pages/Sales/PDV/Payment/index.js
+++ src/pages/Sales/PDV/Payment/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { MdDone } from 'react-icons/md';
 
-import { Container } from './styles';
+import BoxPDV from '../../../../components/Box_point-sale';
+import BoxItens from '../../../../components/Box_itens-sales2';
+
+import { Container, BoxLeft, Content, Footer, BoxRight } from './styles';
 
 function Payment() {
   return (
-    <Container>
-      <h1>Payment</h1>
-    </Container>
+    <BoxPDV poup>
+      <Container>
+        <BoxLeft>
+          <Content>
+            <h1>Box-left</h1>
+          </Content>
+
+          <Footer>
+            <button type="button">
+              <strong>CONFIRMAR PAGAMENTO</strong>
+              <MdDone color="#fff" size={25} />
+            </button>
+          </Footer>
+        </BoxLeft>
+        <BoxRight>
+          <BoxItens />
+        </BoxRight>
+      </Container>
+    </BoxPDV>
   );
 }
 
--- src/pages/Sales/PDV/Payment/styles.js
+++ src/pages/Sales/PDV/Payment/styles.js
@@ -1,3 +1,60 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  min-width: 900px;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BoxLeft = styled.div`
+  background: #fff;
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  padding: 15px;
+`;
+
+export const Content = styled.div``;
+
+export const Footer = styled.div`
+  width: 100%;
+
+  button {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background: #8bc53d;
+    width: 100%;
+    height: 45px;
+    border-radius: 4px;
+    border: none;
+
+    :hover {
+      background: ${darken(0.08, '#8bc53d')};
+    }
+
+    strong {
+      font-size: 18px;
+      color: #fff;
+      margin-right: 10px;
+    }
+
+    svg {
+      margin-top: -4px;
+    }
+  }
+`;
+
+export const BoxRight = styled.div`
+  width: 30%;
+  height: 100%;
+`;
