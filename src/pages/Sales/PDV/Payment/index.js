63e9cc08d389291128fa8e98e861a32a84d9fd93
unknown <jeffynho32@gmail.com>
2020-10-29 19:05:19 -0300

    feat: Defindo feature para completar vendas avista

--- src/pages/Sales/PDV/Payment/index.js
+++ src/pages/Sales/PDV/Payment/index.js
@@ -1,26 +1,199 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import { MdDone } from 'react-icons/md';
 
 import BoxPDV from '../../../../components/Box_point-sale';
 import BoxItens from '../../../../components/Box_itens-sales2';
+import BoxSetCustomer from '../../../../components/Box-set_customer';
+import Payment1 from '../../../../components/Payment1';
 
-import { Container, BoxLeft, Content, Footer, BoxRight } from './styles';
+import { completeToSaleRequest } from '../../../../store/module/sale/actions';
+
+import {
+  Container,
+  BoxLeft,
+  Content,
+  OptionButton,
+  PaymentSelect,
+  Payment2,
+  Payment3,
+  Footer,
+  BoxRight,
+} from './styles';
 
 function Payment() {
+  const { dataSale } = useSelector((state) => state.saleData);
+  const dispatch = useDispatch();
+
+  const [customer, setCustomer] = useState(false);
+  const [infoTrue, setInfoTrue] = useState(false);
+  const [infoFalse, setInfoFalse] = useState(true);
+  const [animation, setAnimation] = useState(0);
+  const [installments, setInstallments] = useState(0);
+  const [selectedPayment, setSelectedPayment] = useState({
+    id: 1,
+    label: 'AVISTA',
+  });
+
+  const payment = [
+    {
+      id: 1,
+      label: 'AVISTA',
+    },
+    {
+      id: 2,
+      label: 'CARTÃO CRÉDITO',
+    },
+    {
+      id: 3,
+      label: 'CARTÃO DÉBITO',
+    },
+  ];
+
+  function handleInfoTrue() {
+    setInfoTrue(true);
+    setInfoFalse(false);
+    setCustomer(true);
+    setAnimation(0);
+  }
+
+  function handleInfoFalse() {
+    setInfoFalse(true);
+    setInfoTrue(false);
+    setCustomer(false);
+    setAnimation(0);
+  }
+
+  const renderSetCustomer = useMemo(
+    () => (
+      <BoxSetCustomer
+        handleInfoFalse={() => handleInfoFalse()}
+        animation={animation}
+        setAnimation={() => {
+          setAnimation(1);
+        }}
+      />
+    ),
+    [animation]
+  );
+
+  const renderPayment = useMemo(() => {
+    if (selectedPayment.id === 1) {
+      return (
+        <Payment1>
+          <strong>VALOR PAGO</strong>
+
+          <div className="box-money" />
+        </Payment1>
+      );
+    }
+
+    if (selectedPayment.id === 2) {
+      return (
+        <Payment2>
+          <h1>Payment 2</h1>
+        </Payment2>
+      );
+    }
+
+    if (selectedPayment.id === 3) {
+      return (
+        <Payment3>
+          <h1>Payment 3</h1>
+        </Payment3>
+      );
+    }
+    return this;
+  }, [selectedPayment]);
+
+  function completeToSale() {
+    const data = {
+      payment: selectedPayment.id,
+      sale_id: dataSale.id,
+      installments,
+    };
+
+    confirmAlert({
+      title: 'COMPLETAR A VENDA',
+      message: 'DESEJA REALMENTE COMPLETAR A VENDA?',
+      buttons: [
+        {
+          label: 'SIM',
+          onClick: async () => {
+            dispatch(completeToSaleRequest(data));
+          },
+        },
+        {
+          label: 'NÃO',
+          onClick: () => {},
+        },
+      ],
+    });
+  }
+
   return (
     <BoxPDV poup>
       <Container>
         <BoxLeft>
           <Content>
-            <h1>Box-left</h1>
+            <div className="box-info-customers">
+              <h2>INFORMAR CLIENTE ?</h2>
+              <div className="box-options">
+                <OptionButton
+                  type="button"
+                  className="option-block"
+                  poup={infoTrue}
+                  onClick={handleInfoTrue}
+                >
+                  <div className="option-container">
+                    <div className="option-content" />
+                  </div>
+                  <strong>SIM</strong>
+                </OptionButton>
+
+                <OptionButton
+                  type="button"
+                  className="option-block"
+                  poup={infoFalse}
+                  onClick={handleInfoFalse}
+                >
+                  <div className="option-container">
+                    <div className="option-content" />
+                  </div>
+                  <strong>NÃO</strong>
+                </OptionButton>
+              </div>
+            </div>
+
+            <div className="box-payment">
+              <strong>FORMA DE PAGAMENTO:</strong>
+              <PaymentSelect
+                value={selectedPayment}
+                options={payment}
+                getOptionValue={(op) => op.id}
+                getOptionLabel={(op) => op.label}
+                onChange={(value) => {
+                  setSelectedPayment({
+                    id: value.id,
+                    label: value.label,
+                  });
+                }}
+              />
+            </div>
+
+            {renderPayment}
           </Content>
 
           <Footer>
-            <button type="button">
+            <button type="button" onClick={completeToSale}>
               <strong>CONFIRMAR PAGAMENTO</strong>
               <MdDone color="#fff" size={25} />
             </button>
           </Footer>
+
+          {customer && renderSetCustomer}
         </BoxLeft>
         <BoxRight>
           <BoxItens />
--- src/pages/Sales/PDV/Payment/styles.js
+++ src/pages/Sales/PDV/Payment/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { darken } from 'polished';
+import Async from 'react-select';
 
 export const Container = styled.div`
   width: 100%;
@@ -22,7 +23,88 @@ export const BoxLeft = styled.div`
   padding: 15px;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  div.box-info-customers {
+    h2 {
+      font-size: 15px;
+      color: #666;
+    }
+
+    div.box-options {
+      margin-top: 5px;
+
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      width: 120px;
+    }
+  }
+
+  div.box-payment {
+    margin-top: 15px;
+
+    strong {
+      font-size: 18px;
+      color: #666;
+    }
+  }
+`;
+
+export const OptionButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background: none;
+  border: none;
+  cursor: default;
+
+  div.option-container {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    border: 2px solid #ab0000;
+    margin-right: 5px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    div.option-content {
+      width: 7px;
+      height: 7px;
+      border-radius: 50%;
+      background: ${(props) => (props.poup ? '#ab0000' : '#fff')};
+    }
+  }
+`;
+
+export const PaymentSelect = styled(Async).attrs({
+  styles: {
+    option: (provided, state) => ({
+      ...provided,
+      color: state.isFocused ? '#fff' : '#444',
+      background: state.isFocused ? '#ab0000' : '#fff',
+    }),
+    control: () => ({
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+      display: 'flex',
+      alignItems: 'center',
+      height: '35px',
+      background: '#eee',
+    }),
+  },
+})`
+  margin-top: 5px;
+`;
+
+export const Payment1 = styled.div``;
+
+export const Payment2 = styled.div``;
+
+export const Payment3 = styled.div``;
 
 export const Footer = styled.div`
   width: 100%;
