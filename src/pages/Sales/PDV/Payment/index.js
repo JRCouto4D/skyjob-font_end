25b45cf6aa7d8d2d12063b6b8cd6ebe193b74475
unknown <jeffynho32@gmail.com>
2020-10-30 19:44:04 -0300

    feat(pages/Sales/PDV/Payment): Definindo métodos para pagamentos com cartão de crédito e débito

--- src/pages/Sales/PDV/Payment/index.js
+++ src/pages/Sales/PDV/Payment/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React, { useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { confirmAlert } from 'react-confirm-alert';
@@ -8,6 +9,9 @@ import BoxPDV from '../../../../components/Box_point-sale';
 import BoxItens from '../../../../components/Box_itens-sales2';
 import BoxSetCustomer from '../../../../components/Box-set_customer';
 import Payment1 from '../../../../components/Payment1';
+import Payment2 from '../../../../components/Payment2';
+
+import { formatPrice } from '../../../../util/format';
 
 import { completeToSaleRequest } from '../../../../store/module/sale/actions';
 
@@ -17,21 +21,20 @@ import {
   Content,
   OptionButton,
   PaymentSelect,
-  Payment2,
-  Payment3,
   Footer,
   BoxRight,
 } from './styles';
 
 function Payment() {
-  const { dataSale } = useSelector((state) => state.saleData);
+  const { dataSale, installments } = useSelector((state) => state.saleData);
   const dispatch = useDispatch();
 
   const [customer, setCustomer] = useState(false);
+  const [viewPayment2, setViewPayment2] = useState(false);
   const [infoTrue, setInfoTrue] = useState(false);
   const [infoFalse, setInfoFalse] = useState(true);
   const [animation, setAnimation] = useState(0);
-  const [installments, setInstallments] = useState(0);
+  const [animation2, setAnimation2] = useState(1);
   const [selectedPayment, setSelectedPayment] = useState({
     id: 1,
     label: 'AVISTA',
@@ -79,40 +82,114 @@ function Payment() {
     [animation]
   );
 
+  function handleview() {
+    setSelectedPayment({
+      id: 1,
+      label: 'AVISTA',
+    });
+    setViewPayment2(false);
+    setAnimation2(1);
+  }
+
+  function handleview2() {
+    setViewPayment2(false);
+    setAnimation2(1);
+  }
+
+  const renderPayment2 = useMemo(() => {
+    if (selectedPayment.id === 2) {
+      return (
+        <Payment2
+          handleview={() => handleview()}
+          handleview2={() => handleview2()}
+          animation={animation2}
+          funcCancel={() => {
+            setAnimation2(3);
+          }}
+          funcOk={() => {
+            setAnimation2(2);
+          }}
+        />
+      );
+    }
+    return this;
+  }, [animation2, selectedPayment]);
+
   const renderPayment = useMemo(() => {
     if (selectedPayment.id === 1) {
-      return (
-        <Payment1>
-          <strong>VALOR PAGO</strong>
-
-          <div className="box-money" />
-        </Payment1>
-      );
+      return <Payment1 />;
     }
 
     if (selectedPayment.id === 2) {
       return (
-        <Payment2>
-          <h1>Payment 2</h1>
-        </Payment2>
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: 50,
+          }}
+        >
+          <strong
+            style={{
+              fontSize: 20,
+              color: '#666',
+              display: 'block',
+            }}
+          >
+            {installments
+              ? `PAGAMENTO EM ${installments.label} NO CARTÃO DE CRÉDITO`
+              : ''}
+          </strong>
+          <span
+            style={{
+              color: '#999',
+              fontSize: 12,
+              fontWeight: 'bold',
+              marginTop: '3px',
+              display: 'block',
+            }}
+          >
+            {dataSale && installments
+              ? installments.installments === 1
+                ? `${installments.installments} PARCELA DE ${formatPrice(
+                    dataSale.total / installments.installments
+                  )}`
+                : `${installments.installments} PARCELAS DE ${formatPrice(
+                    dataSale.total / installments.installments
+                  )}`
+              : ''}
+          </span>
+        </div>
       );
     }
 
     if (selectedPayment.id === 3) {
       return (
-        <Payment3>
-          <h1>Payment 3</h1>
-        </Payment3>
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: 50,
+          }}
+        >
+          <strong
+            style={{
+              fontSize: 20,
+              color: '#666',
+              display: 'block',
+            }}
+          >
+            PAGAMENTO COM CARTÃO DE DÉBITO
+          </strong>
+        </div>
       );
     }
     return this;
-  }, [selectedPayment]);
+  }, [selectedPayment, installments]);
 
   function completeToSale() {
     const data = {
       payment: selectedPayment.id,
       sale_id: dataSale.id,
-      installments,
+      installments: installments ? installments.installments : 0,
     };
 
     confirmAlert({
@@ -179,6 +256,8 @@ function Payment() {
                     id: value.id,
                     label: value.label,
                   });
+                  setAnimation(1);
+                  setViewPayment2(true);
                 }}
               />
             </div>
@@ -194,6 +273,8 @@ function Payment() {
           </Footer>
 
           {customer && renderSetCustomer}
+
+          {selectedPayment.id === 2 && viewPayment2 && renderPayment2}
         </BoxLeft>
         <BoxRight>
           <BoxItens />
