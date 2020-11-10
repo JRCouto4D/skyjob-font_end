75b00419631ea661d5ff79bec12224ac3019cfe0
unknown <jeffynho32@gmail.com>
2020-11-09 22:53:46 -0300

    feat(Catalog/Customers): Definindo estilos e listagem de clientes

--- src/pages/Catalog/Customers/index.js
+++ src/pages/Catalog/Customers/index.js
@@ -1,11 +1,180 @@
-import React from 'react';
+/* eslint-disable no-nested-ternary */
+import React, { useState, useMemo, useEffect, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  MdSearch,
+  MdCreate,
+  MdDeleteForever,
+  MdFastRewind,
+  MdFastForward,
+} from 'react-icons/md';
+import { FaSpinner } from 'react-icons/fa';
+import { Input } from '@rocketseat/unform';
 
-import { Container } from './styles';
+import api from '../../../services/api';
+
+import {
+  Container,
+  Content,
+  Header,
+  TableCustomer,
+  LineTableCustomers,
+  BoxNavigation,
+  Loading,
+} from './styles';
 
 function Customers() {
+  const [search, setSearch] = useState([]);
+  const [page, setPage] = useState(1);
+  const [prePage, setPrePage] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [customers, setCustomers] = useState([]);
+
+  const { company } = useSelector((state) => state.user.profile);
+
+  const loadCustomers = useCallback(async () => {
+    setLoading(true);
+
+    const response = await api.get(`company/${company.id}/customers/list`, {
+      params: {
+        page,
+        name: search,
+      },
+    });
+
+    setCustomers(response.data.customers);
+    setTotal(response.data.total);
+    setPrePage(Math.ceil(response.data.total / 5));
+    setLoading(false);
+  }, [page, search, company]);
+
+  useEffect(() => {
+    loadCustomers();
+  }, [search, loadCustomers]);
+
+  function nextPage() {
+    setPrePage(Math.ceil(total / 5));
+    setPage(page < prePage ? page + 1 : page);
+
+    loadCustomers();
+  }
+
+  function prevPage() {
+    setPage(page >= 2 ? page - 1 : page);
+
+    loadCustomers();
+  }
+
+  const tableCustomers = useMemo(
+    () => (
+      <TableCustomer>
+        <li className="header">
+          <strong>NOME</strong>
+          <strong>CPF</strong>
+          <strong>TELEFONE</strong>
+          <strong>EMAIL</strong>
+          <strong>AÇÕES</strong>
+        </li>
+        {customers.map((customer) => (
+          <LineTableCustomers>
+            <strong>{customer.name}</strong>
+            <strong>{customer.cpf}</strong>
+            <strong>{customer.telephone}</strong>
+            <strong>{customer.email}</strong>
+            <div className="box-actions">
+              <button type="button">
+                <MdCreate size={20} color="#333" />
+              </button>
+
+              <button type="button">
+                <MdDeleteForever size={20} color="#ab0000" />
+              </button>
+            </div>
+          </LineTableCustomers>
+        ))}
+      </TableCustomer>
+    ),
+    [customers]
+  );
+
   return (
     <Container>
-      <h1>Customers</h1>
+      <Content>
+        <div>
+          <Header>
+            <div className="header-box-left">
+              <strong>CATÁLOGO</strong>
+              <h1>CLIENTES</h1>
+            </div>
+
+            <div className="box-search">
+              <MdSearch size={20} color="#ccc" />
+
+              <Input
+                id="search"
+                name="search"
+                placeholder="O que voce está procurando"
+                autoCapitalize="none"
+                autoComplete="none"
+                onChange={(e) => {
+                  setPage(1);
+                  setSearch(e.target.value);
+                }}
+              />
+            </div>
+
+            <button type="button">
+              <span>ADICIONAR</span>
+              <strong>NOVO</strong>
+            </button>
+          </Header>
+
+          <div className="box-table-customers">
+            {loading ? (
+              <Loading>
+                <FaSpinner color="#ab0000" size={30} />
+              </Loading>
+            ) : customers.length >= 1 ? (
+              tableCustomers
+            ) : (
+              <div
+                style={{
+                  marginTop: 20,
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <h1
+                  style={{
+                    color: '#666',
+                  }}
+                >
+                  NENHUM RESULTADO ENCONTRADO
+                </h1>
+              </div>
+            )}
+            <BoxNavigation>
+              <div>
+                <button onClick={prevPage} type="button" disabled={page === 1}>
+                  <MdFastRewind size={20} color="#fff" />
+                </button>
+
+                <span>{`${page} de ${prePage}`}</span>
+
+                <button
+                  onClick={nextPage}
+                  type="button"
+                  disabled={page >= prePage}
+                >
+                  <MdFastForward size={20} color="#fff" />
+                </button>
+              </div>
+            </BoxNavigation>
+          </div>
+        </div>
+      </Content>
     </Container>
   );
 }
--- src/pages/Catalog/Customers/styles.js
+++ src/pages/Catalog/Customers/styles.js
@@ -1,3 +1,203 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  width: 80%;
+  height: 475px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 15px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  div.box-table-customers {
+    min-height: 395px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  div.header-box-left {
+    display: flex;
+    flex-direction: column;
+
+    strong {
+      font-size: 16px;
+      color: #172c3d;
+      font-weight: bold;
+    }
+
+    h1 {
+      color: #ab0000;
+    }
+  }
+
+  div.box-search {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    height: 40px;
+    margin-left: 20px;
+
+    svg {
+      margin-left: 10px;
+    }
+
+    input {
+      height: 22px;
+      width: 100%;
+      border: none;
+      border-left: 1px solid #ccc;
+      margin-left: 10px;
+      padding-left: 10px;
+      color: #999;
+      ::placeholder {
+        color: #ccc;
+      }
+    }
+  }
+
+  button {
+    margin-left: 20px;
+    background: #ab0000;
+    border: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    padding: 8px 20px;
+
+    span {
+      font-size: 12px;
+      color: #fff;
+      font-weight: bold;
+    }
+
+    strong {
+      font-size: 18px;
+      color: #fff;
+    }
+  }
+`;
+
+export const TableCustomer = styled.ul`
+  width: 100%;
+  display: grid;
+  grid-gap: 15px;
+  margin-top: 25px;
+
+  li.header {
+    margin-top: 3px;
+    display: grid;
+    grid-template-columns: 4fr 2fr 2fr 2fr 60px;
+    padding: 0 10px;
+
+    strong {
+      color: #333;
+      font-size: 12.5px;
+    }
+  }
+`;
+
+export const LineTableCustomers = styled.li`
+  display: grid;
+  grid-template-columns: 4fr 2fr 2fr 2fr 60px;
+  background: #eee;
+  width: 100%;
+  height: 45px;
+  padding: 12px 10px;
+  align-items: center;
+  border-radius: 4px;
+  text-align: left;
+  strong {
+    color: #666;
+    font-weight: normal;
+    font-size: 12px;
+  }
+  div.box-actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 50px;
+    button {
+      background: none;
+      border: none;
+      :hover {
+        opacity: 0.7;
+      }
+    }
+  }
+`;
+
+export const BoxNavigation = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 10px;
+
+  div {
+    display: flex;
+    width: 150px;
+    align-items: center;
+    justify-content: space-between;
+    button {
+      background: #ab0000;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: none;
+      padding: 8px;
+      :disabled {
+        opacity: 0.4;
+        pointer-events: none;
+      }
+    }
+    span {
+      color: #666;
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+`;
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg)
+  }
+  to{
+    transform: rotate(360deg)
+  }
+`;
+
+export const Loading = styled.div`
+  width: 100%;
+  height: 325px;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    animation: ${rotate} 1s linear infinite;
+  }
+`;
