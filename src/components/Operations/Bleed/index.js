23db37b70fbbd8d02db35e60527962df9b6a6bc1
unknown <jeffynho32@gmail.com>
2020-11-21 22:19:02 -0300

    feat: Definindo estilos para componente que vai lidar com sangrias no caixa

--- src/components/Operations/Bleed/index.js
+++ src/components/Operations/Bleed/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { Input } from '@rocketseat/unform';
+import { MdForward } from 'react-icons/md';
 
-import { Container } from './styles';
+import left from '../../../assets/left.png';
+
+import { Container, Content } from './styles';
 
 function Bleed() {
   return (
     <Container>
-      <h1>Bleed</h1>
+      <Content>
+        <header />
+        <footer>
+          <div className="box-body">
+            <div className="box-left">
+              <strong>VALOR EM DINHEIRO</strong>
+              <div className="box-input">
+                <strong>R$</strong>
+                <Input
+                  type="number"
+                  id="money"
+                  name="money"
+                  placeholder="0.00"
+                />
+              </div>
+            </div>
+
+            <img src={left} alt="left" />
+
+            <div className="box-label">
+              <strong>SANGRIA DO CAIXA</strong>
+              <span>DIGITE O VALOR QUE SERÁ RETIRADO DO CAIXA</span>
+            </div>
+          </div>
+
+          <button type="button" onClick={() => {}}>
+            <MdForward size={45} color="#fff" />
+            <strong>CONFIMAR SANGRIA</strong>
+          </button>
+        </footer>
+      </Content>
     </Container>
   );
 }
--- src/components/Operations/Bleed/styles.js
+++ src/components/Operations/Bleed/styles.js
@@ -1,3 +1,98 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-top: 130px;
+  height: 100%;
+`;
+
+export const Content = styled.div`
+  width: 658px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  footer {
+    div.box-body {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-end;
+      justify-content: space-between;
+
+      div.box-left {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+
+        strong {
+          font-size: 20px;
+          color: #666;
+        }
+
+        div.box-input {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          margin-top: 5px;
+          width: 250px;
+          height: 60px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          padding-left: 10px;
+
+          strong {
+            font-size: 35px;
+            color: #ccc;
+          }
+
+          input {
+            width: 100%;
+            height: 100%;
+            border: none;
+            font-size: 35px;
+            color: #ccc;
+            margin-left: 5px;
+
+            ::placeholder {
+              color: #ccc;
+            }
+          }
+        }
+      }
+
+      div.box-label {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        strong {
+          font-size: 30px;
+          color: #666;
+        }
+
+        span {
+          font-size: 11px;
+          color: #999;
+        }
+      }
+    }
+
+    button {
+      width: 100%;
+      height: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #8bc53d;
+      border: none;
+      border-radius: 4px;
+      margin-top: 25px;
+
+      strong {
+        font-size: 30px;
+        color: #fff;
+        margin-left: 15px;
+      }
+    }
+  }
+`;
