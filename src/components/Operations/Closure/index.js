5eae9c38dec3be8f1123e8358bfb330f047103ab
unknown <jeffynho32@gmail.com>
2020-11-25 14:12:25 -0300

    feat: Defindo estilos para o componente que vai lidar com fechamento do caixa

--- src/components/Operations/Closure/index.js
+++ src/components/Operations/Closure/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { FaSpinner } from 'react-icons/fa';
+import { MdForward } from 'react-icons/md';
 
-import { Container } from './styles';
+import { formatPrice } from '../../../util/format';
+
+import { Container, Content, Loading } from './styles';
 
 function Closure() {
+  const { pdv } = useSelector((state) => state.statusPDV);
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleClosure() {
+    setLoading(!loading);
+  }
+
   return (
     <Container>
-      <h1>Closure</h1>
+      <Content>
+        <header>
+          <strong style={{ color: '#ccc' }}>{`PDV: #${pdv.id}`}</strong>
+          <strong>{`TOTAL DE VENDAS: ${16}`}</strong>
+        </header>
+
+        <div className="box-main">
+          <div className="label-block">
+            <strong>PAGAMENTOS EM DINHEIRO</strong>
+            <span>{`TOTAL: ${formatPrice(510)}`}</span>
+          </div>
+
+          <div className="label-block">
+            <strong>PAGAMENTOS COM CATÃO DE CRÉDITO</strong>
+            <span>{`TOTAL: ${formatPrice(811.5)}`}</span>
+          </div>
+
+          <div className="label-block">
+            <strong>PAGAMENTOS COM CATÃO DE DÉBITO</strong>
+            <span>{`TOTAL: ${formatPrice(214)}`}</span>
+          </div>
+        </div>
+
+        <button type="button" onClick={handleClosure}>
+          {loading ? (
+            <Loading>
+              <FaSpinner color="#fff" size={20} />
+            </Loading>
+          ) : (
+            <>
+              <MdForward color="#fff" size={45} />
+              <strong>CONFIMER FECHAMENTO</strong>
+            </>
+          )}
+        </button>
+      </Content>
     </Container>
   );
 }
