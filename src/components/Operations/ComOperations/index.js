ecdd6ff7a41130b91881079b42ea2a286fb1ee90
unknown <jeffynho32@gmail.com>
2020-11-27 02:09:44 -0300

    feat: Definindo estilos para os filtros de resultado

--- src/components/Operations/ComOperations/index.js
+++ src/components/Operations/ComOperations/index.js
@@ -1,11 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdFilterList, MdDone } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Content, Filters } from './styles';
 
 function ComOperations() {
+  const [opened, setOpened] = useState(true);
+  const [bleed, setBleed] = useState(true);
+  const [reinforcement, setReinforcement] = useState(true);
+  const [closure, setClosure] = useState(true);
+
+  function handleOptions(op) {
+    switch (op) {
+      case 1: {
+        setOpened(!opened);
+        break;
+      }
+
+      case 2: {
+        setBleed(!bleed);
+        break;
+      }
+
+      case 3: {
+        setReinforcement(!reinforcement);
+        break;
+      }
+
+      case 4: {
+        setClosure(!closure);
+        break;
+      }
+      default:
+    }
+  }
+
   return (
     <Container>
-      <h1>Operations</h1>
+      <Content>
+        <Filters>
+          <MdFilterList color="#fff" size={30} />
+
+          <div className="box-options">
+            <div className="option-block">
+              <button
+                type="button"
+                className="option-button"
+                onClick={() => handleOptions(1)}
+              >
+                <div className="option-container">
+                  {opened && (
+                    <div className="option-content">
+                      <MdDone color="#fff" size={12} />
+                    </div>
+                  )}
+                </div>
+
+                <strong>ABERTURA</strong>
+              </button>
+            </div>
+
+            <div className="option-block">
+              <button
+                type="button"
+                className="option-button"
+                onClick={() => handleOptions(2)}
+              >
+                <div className="option-container">
+                  {bleed && (
+                    <div className="option-content">
+                      <MdDone color="#fff" size={12} />
+                    </div>
+                  )}
+                </div>
+
+                <strong>SANGRIA</strong>
+              </button>
+            </div>
+
+            <div className="option-block">
+              <button
+                type="button"
+                className="option-button"
+                onClick={() => handleOptions(3)}
+              >
+                <div className="option-container">
+                  {reinforcement && (
+                    <div className="option-content">
+                      <MdDone color="#fff" size={12} />
+                    </div>
+                  )}
+                </div>
+
+                <strong>REFORÇO</strong>
+              </button>
+            </div>
+
+            <div className="option-block">
+              <button
+                type="button"
+                className="option-button"
+                onClick={() => handleOptions(4)}
+              >
+                <div className="option-container">
+                  {closure && (
+                    <div className="option-content">
+                      <MdDone color="#fff" size={12} />
+                    </div>
+                  )}
+                </div>
+
+                <strong>FECHAMENTO</strong>
+              </button>
+            </div>
+          </div>
+        </Filters>
+      </Content>
     </Container>
   );
 }
--- src/components/Operations/ComOperations/styles.js
+++ src/components/Operations/ComOperations/styles.js
@@ -1,3 +1,74 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-top: 140px;
+  height: 100%;
+`;
+
+export const Content = styled.div`
+  width: 658px;
+`;
+
+export const Filters = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  width: 658px;
+  height: 45px;
+
+  background: #333;
+  border-radius: 4px;
+  padding: 0 15px 0 10px;
+
+  div.box-options {
+    width: 100%;
+    margin-left: 20px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    div.option-block {
+      button {
+        background: none;
+        border: none;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        div.option-container {
+          width: 18px;
+          height: 18px;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          background: #fff;
+          border-radius: 2px;
+
+          div.option-content {
+            width: 14px;
+            height: 14px;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            border-radius: 2px;
+            background: #00bfdd;
+          }
+        }
+
+        strong {
+          color: #fff;
+          font-size: 12px;
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+`;
