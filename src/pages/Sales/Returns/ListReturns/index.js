9a25bcb8d28b59dea54313b62358efb82fb23f2e
unknown <jeffynho32@gmail.com>
2020-12-16 23:38:46 -0300

    feat: Iniciando página que vai lidar com listagem de vendas estornadas

---
+++ src/pages/Sales/Returns/ListReturns/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function ListReturns() {
+  return (
+    <Container>
+      <h1>ListReturns</h1>
+    </Container>
+  );
+}
+
+export default ListReturns;
