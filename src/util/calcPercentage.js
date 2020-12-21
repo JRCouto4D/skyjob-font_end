5eb01132ce3462281bd53c43ea8b2316a012fff6
unknown <jeffynho32@gmail.com>
2020-12-20 23:10:36 -0300

    feat: incluindo função para auxiliar no calculo de porcetagens

--- src/util/calcPercentage.js
+++ src/util/calcPercentage.js
@@ -11,3 +11,10 @@ export function percentage2(a, b) {
   const e = 100 - d;
   return e.toFixed(1);
 }
+
+export function percentage3(a, b) {
+  const c = b.toFixed(2) * 100;
+  const d = c / a.toFixed(2);
+
+  return d.toFixed(1);
+}
