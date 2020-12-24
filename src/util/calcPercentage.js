65ff23f974212334a7ff0d823634fad59bc48efb
unknown <jeffynho32@gmail.com>
2020-12-24 18:29:24 -0300

    feat: features para facilitar calculos de porcetagens e formatação de moedas

--- src/util/calcPercentage.js
+++ src/util/calcPercentage.js
@@ -6,6 +6,10 @@ export function percentage(a, b) {
 }
 
 export function percentage2(a, b) {
+  if (a <= 0 || b <= 0) {
+    return 100;
+  }
+
   const c = b.toFixed(2) * 100;
   const d = c / a.toFixed(2);
   const e = 100 - d;
@@ -17,4 +21,11 @@ export function percentage3(a, b) {
   const d = c / a.toFixed(2);
 
   return d.toFixed(1);
+}
+
+export function percentage4(a, b) {
+  const c = b.toFixed(2) * 100;
+  const d = c / a.toFixed(2);
+  const e = d - 100;
+  return e.toFixed(1);
 }
--- src/util/format.js
+++ src/util/format.js
@@ -2,3 +2,10 @@ export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
   style: 'currency',
   currency: 'BRL',
 });
+
+export const { format: formatPriceNoDollarSign } = new Intl.NumberFormat(
+  'pt-BR',
+  {
+    minimumFractionDigits: 2,
+  }
+);
