06111d80eb654e3489a96e8d94d7c26f67162d25
unknown <jeffynho32@gmail.com>
2020-12-30 02:00:49 -0300

    feat: Incluindo função que vai lidar com a movimentação no estoque

--- src/pages/Catalog/Products/StockMoviment/index.js
+++ src/pages/Catalog/Products/StockMoviment/index.js
@@ -3,10 +3,12 @@ import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { MdClear, MdSwapHoriz } from 'react-icons/md';
 import { Input } from '@rocketseat/unform';
+import { toast } from 'react-toastify';
+import { lighten } from 'polished';
 
 import history from '../../../../services/history';
 
-// import { formatPrice } from '../../../../util/format';
+import api from '../../../../services/api';
 
 import { Modal, Container, Content, Select } from './styles';
 
@@ -17,6 +19,7 @@ function StockMoviment({ location }) {
   const [animation, setAnimation] = useState(0);
   const [selectedMoviment, setSelectedMoviment] = useState(0);
   const [amount, setAmount] = useState('');
+  const [error, setError] = useState(0);
 
   const moviments = [
     {
@@ -38,10 +41,62 @@ function StockMoviment({ location }) {
   }
 
   function handleOk() {
+    async function movimentStock() {
+      try {
+        await api.put(`/moviment_stock/product/${product.id}`, {
+          moviment: selectedMoviment.id,
+          amount: Number(amount),
+        });
+
+        toast.success('O estoque do produto foi movimentado.');
+        setAmount('');
+        setError(0);
+        setSelectedMoviment(0);
+        history.goBack();
+      } catch (err) {
+        toast.error('Algo deu errado e não possível movimentar o estoque.');
+        setAmount('');
+        setError(0);
+        setSelectedMoviment(0);
+        history.goBack();
+      }
+    }
+
+    if (selectedMoviment === 0) {
+      setError(1);
+
+      toast.error('Informe o movimento no estoque.');
+      return;
+    }
+
+    if (amount === '' || amount === 0) {
+      setError(2);
+      const input = document.getElementById('amount');
+      input.focus();
+      input.style.borderColor = '#FF1E40';
+      input.style.background = lighten(0.4, '#FF1E40');
+
+      toast.error('A quantidade a movimentar no estoque é obrigatória.');
+      return;
+    }
+
+    if (selectedMoviment.id === 2 && product.amount_stock < amount) {
+      setError(2);
+      const input = document.getElementById('amount');
+      input.focus();
+      input.style.borderColor = '#FF1E40';
+      input.style.background = lighten(0.4, '#FF1E40');
+
+      toast.error(
+        'A quantidade do movimento de saída é maior que a quantidade em estoque.'
+      );
+      return;
+    }
+
     setAnimation(2);
 
     setTimeout(() => {
-      history.goBack();
+      movimentStock();
     }, 201);
   }
 
@@ -82,29 +137,56 @@ function StockMoviment({ location }) {
           </div>
 
           <div className="select-block">
-            <strong>MOVIMENTO DE</strong>
+            {error === 1 ? (
+              <strong style={{ color: '#FF1E40' }}>* MOVIMENTO DE</strong>
+            ) : (
+              <strong>MOVIMENTO DE</strong>
+            )}
             <Select
               value={selectedMoviment}
               options={moviments}
               getOptionValue={(op) => op.id}
               getOptionLabel={(op) => op.name}
-              onChange={(value) =>
+              onChange={(value) => {
                 setSelectedMoviment({
                   id: value.id,
                   name: value.name,
-                })
-              }
+                });
+                setError(0);
+                const input = document.getElementById('amount');
+                input.focus();
+                input.style.borderColor = '#ddd';
+                input.style.background = 'none';
+              }}
             />
           </div>
 
           <div className="block">
-            <strong>QUANTIDADE</strong>
+            {error === 2 ? (
+              <strong style={{ color: '#FF1E40' }}>* QUANTIDADE</strong>
+            ) : (
+              <strong>QUANTIDADE</strong>
+            )}
             <Input
               type="number"
               id="amount"
               name="amount"
               value={amount}
-              onChange={(e) => setAmount(e.target.value)}
+              onChange={(e) => {
+                setError(0);
+                setAmount(e.target.value);
+                const input = document.getElementById('amount');
+
+                input.style.borderColor = '#ddd';
+                input.style.background = 'none';
+              }}
+              onKeyPress={(e) => {
+                const key = e.which || e.keyCode;
+
+                if (key === 13) {
+                  handleOk();
+                }
+              }}
               autoComplete="off"
               autoCapitalize="off"
             />
@@ -120,7 +202,7 @@ function StockMoviment({ location }) {
         </Content>
       </Container>
     ),
-    [animation, selectedMoviment]
+    [animation, selectedMoviment, amount, error]
   );
 
   return (
