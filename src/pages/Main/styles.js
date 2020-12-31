96c67a35c3b4de9d3d195db0199a445742466189
unknown <jeffynho32@gmail.com>
2020-12-31 14:38:26 -0300

    refactor: incluindo foco no input de email, ao marcar o tipo de usuário

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,4 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   min-width: 1100px;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
