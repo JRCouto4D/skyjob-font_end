9ec628ecd23adb4d2937ab8964d3adfb0f20d49e
unknown <jeffynho32@gmail.com>
2021-01-02 14:47:01 -0300

    feat: Definindo estilos e comportamento do componente que vai lidar com registro e edição de clientes do skyjob

--- src/pages/Admin/Customers/NewCompanies/index.js
+++ src/pages/Admin/Customers/NewCompanies/index.js
@@ -1,11 +1,252 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { MdPrint } from 'react-icons/md';
+import { FaSpinner } from 'react-icons/fa';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
-// import { Container } from './styles';
+import InputImg from './ImageInput';
+
+import api from '../../../../services/api';
+import history from '../../../../services/history';
+
+import { Container, Content, Header, InputBlock, Loading } from './styles';
+
+const schema = Yup.object().shape({
+  description: Yup.string().required('* O nome do cliente é obrigatório.'),
+  email: Yup.string()
+    .email('* Preencha o campo com um email válido.')
+    .required('* O email é obrigatorio.'),
+  password: Yup.string()
+    .min(6, 'A senha deve ter no mínimo 6 digitos')
+    .required('* A senha é obrigatória.'),
+  confirmPassword: Yup.string()
+    .min(6, 'A senha deve ter no mínimo 6 digitos')
+    .required('* A confirmação da senha é obrigatória.'),
+  avatar_id: Yup.number(),
+});
+
+const schema2 = Yup.object().shape({
+  description: Yup.string().required('* O nome do cliente é obrigatório.'),
+  email: Yup.string()
+    .email('* Preencha o campo com um email válido.')
+    .required('* O email é obrigatorio.'),
+  password: Yup.string(),
+  confirmPassword: Yup.string(),
+  avatar_id: Yup.number(),
+});
 
 function NewCompanies({ location }) {
-  console.tron.log(location.state.customer);
+  if (location.state) console.tron.log(location.state.customer);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  return <div />;
+  function handleCancel() {
+    const input = document.getElementById('description');
+    input.value = '';
+    input.focus();
+
+    const input2 = document.getElementById('email');
+    input2.value = '';
+
+    const input3 = document.getElementById('password');
+    input3.value = '';
+
+    const input4 = document.getElementById('confirmPassword');
+    input4.value = '';
+  }
+
+  async function handleSubmit(data) {
+    setError(false);
+    if (!location.state && data.password !== data.confirmPassword) {
+      setError(true);
+      const input = document.getElementById('password');
+      input.focus();
+
+      toast.error('As senhas não correspondem.');
+      return;
+    }
+
+    const previewData = location.state
+      ? {
+          description: data.description,
+          email: data.email,
+          avatar_id: data.avatar_id,
+        }
+      : {
+          description: data.description,
+          email: data.email,
+          password: data.password,
+          avatar_id: data.avatar_id,
+        };
+
+    if (location.state) {
+      const { customer } = location.state;
+
+      try {
+        setLoading(true);
+
+        await api.put(`/skyjob/companies/${customer.id}`, previewData);
+        handleCancel();
+        toast.success('Cliente Editado com sucesso!');
+        history.push('/admin/customers');
+      } catch (err) {
+        toast.error(
+          'Algo deu errado e o cliente não foi editado. Tente mais tarde.'
+        );
+        handleCancel();
+      }
+    } else {
+      try {
+        setLoading(true);
+
+        await api.post(`/skyjob/companies`, previewData);
+        handleCancel();
+        toast.success('Cliente cadastrado com sucesso!');
+        history.push('/admin/customers');
+      } catch (err) {
+        toast.error(
+          'Algo deu errado e o cliente não foi cadastrado. Tente mais tarde.'
+        );
+        handleCancel();
+      }
+    }
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Header>
+          <div className="header-box-left">
+            <h1>CADASTRO DE CLIENTES</h1>
+            <div className="header-navigation">
+              <Link to="/admin/customers">clientes</Link>
+              <strong>/ cadastro</strong>
+            </div>
+          </div>
+
+          <button type="button" onClick={() => {}}>
+            <MdPrint color="#999" size={20} />
+            <strong>IMPRIMIR</strong>
+          </button>
+        </Header>
+
+        <Form
+          initialData={location.state && location.state.customer}
+          schema={location.state ? schema2 : schema}
+          onSubmit={handleSubmit}
+        >
+          <div className="form-content">
+            <div className="box-img">
+              <InputImg />
+            </div>
+            <div className="box-form">
+              <div
+                style={{
+                  width: '100%',
+                }}
+              >
+                <InputBlock>
+                  <strong>NOME</strong>
+                  <Input
+                    id="description"
+                    name="description"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="none"
+                  />
+                </InputBlock>
+
+                <InputBlock>
+                  <strong>EMAIL</strong>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="none"
+                  />
+                </InputBlock>
+              </div>
+
+              <div
+                style={{
+                  width: '100%',
+                  marginTop: 36,
+                }}
+              >
+                <InputBlock>
+                  {error ? (
+                    <strong style={{ color: '#FF1E40' }}>* SENHA</strong>
+                  ) : (
+                    <strong>SENHA</strong>
+                  )}
+                  <Input
+                    type="password"
+                    id="password"
+                    name="password"
+                    autoCapitalize="off"
+                    autoComplete="off"
+                  />
+                </InputBlock>
+
+                <InputBlock>
+                  {error ? (
+                    <strong style={{ color: '#FF1E40' }}>
+                      * CONFIRMAÇÃO DE SENHA
+                    </strong>
+                  ) : (
+                    <strong>CONFIRMAÇÃO DE SENHA</strong>
+                  )}
+                  <Input
+                    type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    autoCapitalize="off"
+                    autoComplete="off"
+                  />
+                </InputBlock>
+              </div>
+            </div>
+          </div>
+
+          <div className="box-buttons">
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={handleCancel}
+            >
+              CANCELAR
+            </button>
+
+            <button type="submit" className="submit-button">
+              {loading ? (
+                <Loading>
+                  <FaSpinner color="#fff" size={18} />
+                </Loading>
+              ) : (
+                <strong>SALVAR</strong>
+              )}
+            </button>
+          </div>
+        </Form>
+      </Content>
+    </Container>
+  );
 }
 
 export default NewCompanies;
+
+NewCompanies.propTypes = {
+  location: PropTypes.shape(),
+};
+
+NewCompanies.defaultProps = {
+  location: null,
+};
+
+/**
+ *
+ */
--- src/pages/Admin/Customers/NewCompanies/styles.js
+++ src/pages/Admin/Customers/NewCompanies/styles.js
@@ -1,3 +1,193 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #eee;
+  width: 100%;
+  height: 100%;
+  padding: 50px 25px 0px;
+  overflow: auto;
+`;
+
+export const Content = styled.div`
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  margin-top: 20px;
+  min-height: 85vh;
+  display: flex;
+  flex-direction: column;
+
+  form {
+    margin-top: 20px;
+
+    span {
+      color: #de3b3b;
+      font-weight: bold;
+    }
+
+    div.form-content {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: space-between;
+
+      div.box-img {
+        padding-right: 20px;
+      }
+
+      div.box-form {
+        margin-left: 20px;
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: space-between;
+      }
+    }
+
+    div.box-buttons {
+      margin-left: 360px;
+      display: flex;
+      align-items: center;
+
+      button.cancel-button {
+        border: 1px solid #ddd;
+        border-radius: 3px;
+        background: #fff;
+        height: 35px;
+        padding: 0 15px;
+        color: #666;
+        margin-left: 1px;
+        transition: 0.2s;
+
+        :hover {
+          background: ${darken(0.03, '#fff')};
+        }
+      }
+
+      button.submit-button {
+        background: #40e0d0;
+        height: 35px;
+        min-width: 90px;
+        padding: 0 15px;
+        border: none;
+        border-radius: 3px;
+        margin-left: 10px;
+        transition: 0.2s;
+
+        :hover {
+          background: ${darken(0.1, '#40e0d0')};
+        }
+
+        strong {
+          color: #fff;
+          font-weight: normal;
+        }
+      }
+    }
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #eee;
+
+  div.header-box-left {
+    h1 {
+      color: #ccc;
+      font-weight: normal;
+    }
+
+    div.header-navigation {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      a {
+        color: #666;
+        margin-top: 2px;
+
+        :hover {
+          text-decoration: underline;
+        }
+      }
+
+      strong {
+        color: #999;
+        margin-top: 2px;
+        margin-left: 5px;
+      }
+    }
+  }
+
+  > button {
+    background: none;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 3px 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.2s;
+
+    :hover {
+      opacity: 0.7;
+    }
+
+    strong {
+      font-size: 12px;
+      color: #999;
+      margin-left: 10px;
+    }
+  }
+`;
+
+export const InputBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 18px;
+
+  strong {
+    font-size: 14px;
+    font-weight: normal;
+    color: #999;
+  }
+
+  input {
+    display: flex;
+    width: 100%;
+    height: 35px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    padding: 0 10px;
+    margin-top: 5px;
+
+    color: #999;
+  }
+`;
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg)
+  }
+  to{
+    transform: rotate(360deg)
+  }
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    animation: ${rotate} 1s linear infinite;
+  }
+`;
