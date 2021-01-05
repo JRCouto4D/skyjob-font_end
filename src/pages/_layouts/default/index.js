f742bc76b79f27492564218f9cf6d860867b6e9b
unknown <jeffynho32@gmail.com>
2021-01-05 13:47:54 -0300

    feat: desabilitando o footer da aplicação

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import RenderHeader from '../../../components/RenderHeader';
-import Footer from '../../../components/Footer';
 import { Wrapper, Content } from './styles';
 
 function deafultLayout({ children }) {
   return (
     <Wrapper>
       <RenderHeader />
-      <Footer />
       <Content>{children}</Content>
     </Wrapper>
   );
